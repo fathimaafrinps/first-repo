228b5c6dabf6a5e2c1c382b548afc7357c59529c
Fathima Afrin <Fathima.Afrin@tekosol.com>
2021-10-13 15:35:03 +0530

    initial

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -10,6 +10,8 @@ using WebApplication1.Models;
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
+
+
     {
         private readonly ILogger<HomeController> _logger;
 
